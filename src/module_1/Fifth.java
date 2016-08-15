d1797777a3ba84c92b85258b825472a94b2c41e5
fabritsievD <fabritsiev_dv@ukr.net>
2016-08-15 21:26:50 +0300

    module_1 commit

---
+++ src/module_1/Fifth.java
@@ -0,0 +1,8 @@
+package module_1;
+
+
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.print("Мой первый класс на Java");
+    }
+}
---
+++ src/module_1/ThirdClass.java
@@ -0,0 +1,8 @@
+package module_1;
+
+
+public class ThirdClass {
+    public static void main(String[] args){
+        System.out.print("мой первый класс на Java");
+    }
+}
