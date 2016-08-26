d01ed852534cb18b5ac9a1015d2d9b740c5b5bc3
fabritsievD <fabritsiev_dv@ukr.net>
2016-08-26 17:35:46 +0300

    MyJavaCorse commit

--- src/module_2/Task_2/Task_2_3.java
+++ src/module_2/Task_2/Task_2_3.java
@@ -5,25 +5,31 @@ public class Task_2_3 {
 
     static double [] balances={1000,2500,400,340,5000};
     static String [] ownerNames={"Vincent","Butch","Marselos","Mia", "Zat"};
-
+    static int i;
+    static double res;
     static double fundBalance(String ownerName, double fund) {
-    int i;
+
+
+
     for( i=0; i<balances.length;i++){
+
         if(ownerNames[i]==ownerName){
-            balances[i]+= fund;
-            break;
+           res= balances[i]+ fund;
+            return res;
         }
-        return -1;
+
             }
-        return balances[i];
+        return -1;
     }
 
     public static void main(String[] args) {
-        String ownerName = "Zati";
-        double fund = 6700;
+        String ownerName = "Butch";
+        double fund = 700;
 
-            if(fundBalance(ownerName,fund)!=(-1))
-        System.out.print(ownerName+" "+fundBalance(ownerName,fund));
+            if(fundBalance(ownerName,fund)!=(-1)) {
+                System.out.print(ownerName + " " + fundBalance(ownerName, fund));
+                balances[i] = res;
+            }
         else System.out.print(ownerName+" NO");
     }
 }
